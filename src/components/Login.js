f2a5f826d1b5609f5564535e79aa4b36ea2a9ac0
Sult Kh <sultkh@Sults-MacBook-Air.local>
2023-07-13 21:46:03 +0800

    major

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,16 +4,16 @@ import {useNavigate, Link} from 'react-router-dom';
 import axios from 'axios';
 export default function Login(){
     const [user, setUser] = useContext(UserContext)
-    const [email, setEmail] = useState('')
+    const [emailInput, setEmail] = useState('')
     const [password, setPassword] = useState('')
     const navigate = useNavigate()
     const submitForm = async (e) => {
         try{
             e.preventDefault()
-            const res = await axios.post('/api/v1/login', {email, password}, {headers: {"Content-Type": "application/json"}})
+            const res = await axios.post('/api/v1/login', { email: emailInput, password: password }, {headers: {"Content-Type": "application/json"}})
             console.log(await res.data)
             const {accessToken, refreshToken, email} = await res.data 
-            if(accessToken && refreshToken){
+            if(accessToken && refreshToken && email){
                 setUser({accessToken, email})
                 localStorage.setItem('refreshToken', refreshToken)
                 return navigate('/')
@@ -25,9 +25,9 @@ export default function Login(){
     return(
         <div className="login-page">
             <form className='login-form' onSubmit={submitForm}>
-                <input placeholder='email' value={email} onChange={(e) => setEmail(e.target.value)}/>
+                <input placeholder='email' value={emailInput} onChange={(e) => setEmail(e.target.value)}/>
                 <input placeholder='password' value={password} onChange={(e) => setPassword(e.target.value)}/>
-                <Link to={'/register'}>regitster</Link>
+                <Link to={'/register'}>register</Link>
                 <button>Login</button>
             </form>
         </div>
